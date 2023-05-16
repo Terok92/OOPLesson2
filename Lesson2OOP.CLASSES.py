5f4bff3d59f4f49a8571b41882dc7113ea145705
Terok92 <tereza.mid@gmail.com>
2023-05-17 03:18:04 +0400

    Add lesson 2

---
+++ Lesson2OOP.CLASSES.py
@@ -0,0 +1,31 @@
+#Task1
+class Person:
+    def __init__(self):
+        self.name = str(input("Enter Persona's name:\t "))
+        self.age = str(input("Enter Persona's age:\t "))
+
+    def display(self):
+        print("Person name is", self.name)
+        print("Person age is ", self.age)
+
+
+class Student(Person):
+    def __init__(self):
+        Person.__init__(self)
+        self.section = str(input("Enter Student's section:\t "))
+
+    def display_Student(self):
+        print("Student section is ", self.section)
+        print("Student name is ", self.name)
+        print("Student age is ", self.age)
+
+
+P = Person()
+P.display()
+S = Student()
+S.display_Student()
+
+
+
+
+
